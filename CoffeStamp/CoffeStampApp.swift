fff65e61874518f35546a80b4973f21622fc545b
EoJin-Kim <62640679+EoJin-Kim@users.noreply.github.com>
2024-03-18 18:10:49 +0900

    Initial Commit

---
+++ CoffeStamp/CoffeStampApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeStampApp.swift
+//  CoffeStamp
+//
+//  Created by kej on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct CoffeStampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeStamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoffeStamp
+//
+//  Created by kej on 3/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
